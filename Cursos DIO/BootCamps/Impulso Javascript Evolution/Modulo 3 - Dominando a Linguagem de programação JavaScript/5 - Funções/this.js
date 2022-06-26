18a167c475877b55e7280b39b31514f9c640b962
alexisxz <alexisxzinho@hotmail.com>
2022-06-26 10:22:57 +0200

    Funcoes e Debugging e Error Handling

---
+++ Cursos DIO/BootCamps/Impulso Javascript Evolution/Modulo 3 - Dominando a Linguagem de programação JavaScript/5 - Funções/this.js
@@ -0,0 +1,30 @@
+const pessoa1 = {
+    nome: "Lucas",
+    idade: 55,
+}
+
+const pessoa2 = {
+    nome: "Maria",
+    idade: 30,
+}
+
+const pessoa3 = {
+    nome: "Carla",
+    idade: 26,
+}
+
+const animal = {
+    nome: "Fiona",
+    idade: 5,
+    raca: "Pug",
+}
+
+
+function calculaIdade(anos) {
+    return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos
+        } anos de idade.`;
+}
+
+console.log(calculaIdade.call(animal, 7)); //dado, parametro
+console.log(calculaIdade.apply(animal, [10])); //dado, parametro
+
--- README.md
+++ README.md
@@ -15,3 +15,4 @@
 - [Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/)
 - [Métodos] (https://www.w3.org/)
 - [Fórum] (https://stackoverflow.com/)
+- [Object error] (https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Error)
